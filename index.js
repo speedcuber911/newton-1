2963e442d66f5f809ac789cf836c04125bfc82f4
Parikshit <parikshit.joon@gmail.com>
2021-12-13 22:30:35 +0530

    (feat): Starting JS

---
+++ index.js
@@ -0,0 +1,13 @@
+// This functions changes color of the button and enters some text there too 
+function clickMe(){      
+        document.write("My first JS program")             
+        document.getElementsByClassName('gridChild2')[0].innerText = 'Loading from external file'        
+        window.alert("Alert message")
+        console.log("Click me function called");
+        window.print();
+    }    
+
+    // innerText
+    // document.write
+    // window.alert
+    // console.log
